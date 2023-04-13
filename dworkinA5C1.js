2f9cb61c556d965b972dcb40c6064f93f5806bd7
liam-dworkin <124173727+liam-dworkin@users.noreply.github.com>
2023-04-13 17:58:55 -0400

    Update dworkinA5C1.js
    Edited it to be a bit less 'framey' and a bit smoother. Boundaries still aren't working.

--- dworkinA5C1.js
+++ dworkinA5C1.js
@@ -1,17 +1,63 @@
+//Liam Dworkin
+//NMD 105
+//April 2023
+
+
+let speedX;
+let x1;
+let directionX;
+
+let speedY;
+let y1;
+let directionY;
+
 function setup() {
-  createCanvas(500, 400);
+  push();
+  frameRate(30);
+  pop();
+  
+  createCanvas(700, 500);
+  
+  speedX = 3;
+  directionX = random(1,5);
+  //x1 = width/2;
+  x1 = random(width);
+
+  speedY = 3;
+  directionY = random(1,5);
+  //y1 = height/2;
+  y1 = random(height);
+  
+  
+  bubble1 = new bubble(random(x1), random(y1), 20);
+  bubble2 = new bubble(random(x1), random(y1), 24);
+  bubble3 = new bubble(random(x1), random(y1), 21);
+  bubble4 = new bubble(random(x1), random(y1), 12);
+  bubble5 = new bubble(random(x1), random(y1), 15);
 }
 
 function draw() {
+  
+  x1 += speedX * directionX;
+  y1 += speedX * directionY;
+
+  if (x1 + 25 >= width || x1 < 25) {
+    // check if hits the right edge
+    directionX *= -1;
+  }
+
+  //conditional - the same as width boundary but for height
+
+  if (y1 + 25 >= height || y1 < 25) {
+    // check if hits the right edge
+    directionY *= -1;
+  }
+
   background("navy");
   //defining the bubble's variables
-  let bubble1 = new bubble(200, 200, 20);
-  let bubble2 = new bubble(120, 300, 24);
-  let bubble3 = new bubble(400, 100, 30);
-  let bubble4 = new bubble(50, 100, 12);
-  let bubble5 = new bubble(250, 300, 15);
 
   //calling back each lil bubble
+  translate (150, 150);
   bubble1.update();
   bubble1.show();
   bubble2.update();
@@ -31,11 +77,12 @@ class bubble {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.rand = random();
   }
   //random variables in x and y (not the best movement yet)
   update() {
-    this.x = this.x + random(10, 20);
-    this.y = this.y + random(0, 10);
+    this.x = this.x + this.rand;
+    this.y = this.y + this.rand;
   }
   show() {
     push();
@@ -44,7 +91,6 @@ class bubble {
     fill("salmon");
     translate(this.x, this.y, this.r * 2);
     ellipse(0, 0, this.r * 3);
-
     pop();
   }
 }

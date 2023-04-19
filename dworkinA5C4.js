0aa23c502d163b2eec4f4d64b000ae1b249cf264
liam-dworkin <124173727+liam-dworkin@users.noreply.github.com>
2023-04-19 17:14:26 -0400

    Update dworkinA5C4.js
    color updates, size updates, speed updates AND the boundaries work!!!
    
    working on adding in hit detection but it was hard to figure out when I couldn't keep anything on the screen without severely braking my code.

--- dworkinA5C4.js
+++ dworkinA5C4.js
@@ -1,6 +1,6 @@
 //Liam Dworkin
 //NMD 105
-//April 15th, 2023
+//April 17th, 2023
 
 let speedX;
 let x1;
@@ -17,7 +17,7 @@ function setup() {
   pop();
   angleMode(DEGREES);
 
-  /*added for random direction and speed of some extent it still doesn't work as I want it to with the boundary boxes. I'm working on that. It also doesn't center as much as I'd like. I think all the random variables are causing some issue that or I can't seem to get it to function properly.*/
+  /*added for random direction and speed of some extent. issues with the boundaries have been solved by removing redundancy and moving all of the conditionals into the classes themselves*/
 
   // speedX = 3;
   // directionX = random(1,3);
@@ -35,13 +35,19 @@ function setup() {
 
   //loop for the bubbles using the array
 
+  //bubble increases the catchers size
+
   for (let bubblesDrawn = 0; bubblesDrawn < 50; bubblesDrawn++) {
-    bubbles[bubblesDrawn] = new bubble(x1, y1, random(8, 13));
+    bubbles[bubblesDrawn] = new bubble(x1, y1, random(7, 10));
   }
 
+  //bubbleBad shrinks the catcher
+
   for (let bubblesDrawn1 = 0; bubblesDrawn1 < 50; bubblesDrawn1++) {
-    bubbles1[bubblesDrawn1] = new bubbleBad(x1, y1, random(8, 13));
+    bubbles1[bubblesDrawn1] = new bubbleBad(x1, y1, random(4, 6));
   }
+
+  //added in the yeetBall (catchers)
 
   for (let yeetBallDrawn = 0; yeetBallDrawn < 3; yeetBallDrawn++) {
     yeetBall1[yeetBallDrawn] = new yeetBall(x1, y1, 1);
@@ -52,19 +58,7 @@ function draw() {
   // x1 += speedX * directionX;
   // y1 += speedY * directionY;
 
-  if (x1 + 25 >= width || x1 < 25) {
-    // check if hits the right edge
-    directionX *= -1;
-  }
-
-  //conditional - the same as width boundary but for height
-
-  if (y1 + 25 >= height || y1 < 25) {
-    // check if hits the right edge
-    directionY *= -1;
-  }
-
-  background("navy");
+  background("lavender");
 
   /*the goal is to have some sort of agar.io like simulation- the bubbles will be collected for enlarging the sprite (yeetBall) and there are particles that will then 'damage' and shrink the yeetBall sprite- I have not yet added in functionality because I wanted to ask about how to do it*/
 
@@ -89,49 +83,38 @@ function draw() {
 
 //code from class assignment for the bubble creation
 
-//still experimenting with boundaries for a ton of random bubbles
-
-//I tried to move the boundary code into the class function
-//It has yet to work
+//boundaries have been solved !!!
+//now they have been moved into the classes themselves
 
 class bubble {
   constructor(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.randX = random(1, 3);
-    this.randY = random(1, 3);
-    this.x1 += random(width);
-    this.y1 += random(height);
-    this.speedX = 3;
-    this.speedY = 3;
+    this.randX = random(1, 5);
+    this.randY = random(1, 5);
   }
-  //random variables in x and y (not the best movement yet)
+
+  //random variables in x and y
+
   update() {
     this.x = this.x + this.randX;
     this.y = this.y + this.randY;
 
-    this.x1 += this.speedX * this.randX;
-    this.y1 += this.speedY * this.randY;
+    //added the class into the conditional itself
 
-    if (this.x1 + 25 >= width || this.x1 < 25) {
+    if (this.x + 25 >= width || this.x < 25) {
       // check if hits the right edge
       this.randX *= -1;
     }
 
     //conditional - the same as width boundary but for height
 
-    if (this.y1 + 25 >= height || this.y1 < 25) {
-      // check if hits the right edge
+    if (this.y + 25 >= height || this.y < 25) {
+      // check if hits the top/bottom edge
       this.randY *= -1;
     }
   }
-
-  /* an experiment
-    move() {
-    x1 += speedX * directionX;
-    y1 += speedX * directionY;
-  } */
 
   show() {
     push();
@@ -149,26 +132,36 @@ class bubbleBad {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.randX = random(1, 3);
+    this.randX = random(1, 5);
     this.randY = random(1, 3);
   }
-  //random variables in x and y (not the best movement yet)
+
+  //random variables in x and y
+
   update() {
     this.x = this.x + this.randX;
     this.y = this.y + this.randY;
-  }
 
-  /* an experiment
-    move() {
-    x1 += speedX * directionX;
-    y1 += speedX * directionY;
-  } */
+    //added the conditional into the class itself
+
+    if (this.x + 25 >= width || this.x < 25) {
+      // check if hits the right edge
+      this.randX *= -1;
+    }
+
+    //conditional - the same as width boundary but for height
+
+    if (this.y + 25 >= height || this.y < 25) {
+      // check if hits the top/bottom edge
+      this.randY *= -1;
+    }
+  }
 
   show() {
     push();
     stroke("green");
     strokeWeight(5);
-    fill("palegoldenrod");
+    fill(random(200));
     translate(this.x, this.y, this.r * 2);
     ellipse(0, 0, this.r * 3);
     pop();
@@ -187,13 +180,27 @@ class yeetBall {
   update() {
     this.x = this.x + this.randX;
     this.y = this.y + this.randY;
+
+    //added the conditional into the class itself
+
+    if (this.x + 25 >= width || this.x < 25) {
+      // check if hits the right edge
+      this.randX *= -1;
+    }
+
+    //conditional - the same as width boundary but for height
+
+    if (this.y + 25 >= height || this.y < 25) {
+      // check if hits the top/bottom edge
+      this.randY *= -1;
+    }
   }
   show() {
     push();
     translate(this.x, this.y, this.r * 2);
-    rotate(random(1,10));
+    rotate(random(1, 10));
     strokeWeight(5);
-    stroke("pink");
+    stroke("violet");
     fill("hsla(170, 80%, 75%, .7)");
     ellipse(0, 0, 80, 80);
     scale(2 / 5);
